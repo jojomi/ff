7c5f8fc0018e2119f956cd8b91da3030d4284d08
jojomi <dev.jojomi@yahoo.com>
2024-11-08 22:18:33 +0100

    Update deps

--- go.mod
+++ go.mod
@@ -1,25 +1,33 @@
 module github.com/jojomi/ff
 
-go 1.16
+go 1.23
 
 require (
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/kylelemons/godebug v1.1.0 // indirect
-	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/lithammer/fuzzysearch v1.1.8
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/mitchellh/mapstructure v1.4.1 // indirect
-	github.com/pelletier/go-toml v1.9.1 // indirect
-	github.com/pkg/errors v0.8.1
-	github.com/sahilm/fuzzy v0.1.0
-	github.com/spf13/afero v1.6.0 // indirect
-	github.com/spf13/cast v1.3.1 // indirect
-	github.com/spf13/cobra v1.1.3
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/viper v1.7.1
-	github.com/stretchr/testify v1.7.0 // indirect
-	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
-	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
-	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
-	golang.org/x/text v0.3.6 // indirect
-	gopkg.in/ini.v1 v1.62.0 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/spf13/cobra v1.8.1
+	github.com/spf13/viper v1.19.0
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/sagikazarmark/locafero v0.6.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.7.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20241108190413-2d47ceb2692f // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -21,9 +21,153 @@
 package main
 
 import (
-	"github.com/jojomi/ff/cmd"
+	"fmt"
+	"io/fs"
+
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/lithammer/fuzzysearch/fuzzy"
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
+var (
+	flagVersion   bool
+	flagVerbose   bool
+	flagCfgFile   string
+	flagFirstOnly bool
+
+	version = "local"
+	commit  = "no-commit"
+	date    = "now"
+)
+
+// RootCmd represents the base command when called without any subcommands.
+var RootCmd = &cobra.Command{
+	Use:   "ff",
+	Short: "find folders that match the given fuzzy search pattern",
+	Run:   rootCmdHandler,
+}
+
+func rootCmdHandler(cmd *cobra.Command, args []string) {
+	// catch version check
+	if flagVersion {
+		fmt.Printf("version %s, build from commit %s at %s\n", version, commit, date)
+		os.Exit(0)
+	}
+
+	if len(args) < 1 {
+		log.Fatal("no search given")
+	}
+	search := args[0]
+	if flagVerbose {
+		fmt.Printf("Searching for: %s\n", search)
+	}
+
+	paths := viper.GetStringSlice("paths")
+	if flagVerbose {
+		fmt.Println("Search base paths:")
+		for _, p := range paths {
+			fmt.Println(p)
+		}
+	}
+
+	subDirs, err := getSubDirs(paths)
+	if err != nil {
+		panic(err)
+	}
+	if flagVerbose {
+		fmt.Println("Folders in search paths:")
+		for _, s := range subDirs {
+			fmt.Println(s)
+		}
+	}
+
+	matches := fuzzy.Find(search, subDirs)
+
+	if flagVerbose {
+		fmt.Println("Results:")
+	}
+
+	for _, match := range matches {
+		fmt.Println(match)
+		if flagFirstOnly {
+			break
+		}
+	}
+}
+
+func getSubDirs(paths []string) ([]string, error) {
+	result := []string{}
+	var (
+		files []fs.DirEntry
+		err   error
+	)
+	for _, path := range paths {
+		path, err = homedir.Expand(path)
+		if err != nil {
+			return []string{}, errors.Wrap(err, "could not expand home dir")
+		}
+
+		if _, err = os.Stat(path); os.IsNotExist(err) {
+			continue
+		}
+
+		files, err = os.ReadDir(path)
+		if err != nil {
+			return []string{}, errors.Wrapf(err, "could not list files in %s", path)
+		}
+
+		for _, f := range files {
+			if !f.IsDir() {
+				continue
+			}
+			result = append(result, filepath.Join(path, f.Name()))
+		}
+	}
+
+	return result, nil
+}
+
 func main() {
-	cmd.Execute()
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	f := RootCmd.PersistentFlags()
+	f.StringVarP(&flagCfgFile, "config", "c", "", "config file (default is $HOME/.ff/config.yml)")
+	f.BoolVarP(&flagFirstOnly, "first-only", "f", false, "print only first element")
+	f.BoolVarP(&flagVerbose, "verbose", "v", false, "print search details")
+	f.BoolVarP(&flagVersion, "version", "", false, "print version details")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if flagCfgFile != "" {
+		viper.SetConfigFile(flagCfgFile)
+	}
+
+	viper.SetConfigName("config")
+	viper.AddConfigPath("$HOME/.ff")
+	viper.AddConfigPath("/etc/ff")
+	viper.AutomaticEnv()
+
+	// If a config file is found, parse it
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if flagVerbose {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
 }
